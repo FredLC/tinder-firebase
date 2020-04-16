8eb31458d26e34c80c09959d0ff78de092ceebb4
fredlc <fredlefevre@outlook.com>
2020-04-16 11:24:57 -0400

    Added rotational transform and card dismiss

--- Tinder/Views/CardView.swift
+++ Tinder/Views/CardView.swift
@@ -11,6 +11,8 @@ import UIKit
 class CardView: UIView {
     
     fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    fileprivate let positiveThreshold: CGFloat = 100
+    fileprivate let negativeThreshold: CGFloat = -100
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,7 +32,7 @@ class CardView: UIView {
         case .changed:
             handleChanged(gesture)
         case .ended:
-            handleEnded()
+            handleEnded(gesture)
         default:
             ()
         }
@@ -39,14 +41,32 @@ class CardView: UIView {
     
     fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
-        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        
+        let rotation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotation.translatedBy(x: translation.x, y: translation.y)
     }
     
     
-    fileprivate func handleEnded() {
-        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.3, options: .curveEaseOut, animations: {
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        let shouldDismissCardRight = gesture.translation(in: nil).x > positiveThreshold
+        let shouldDismissCardLeft = gesture.translation(in: nil).x < negativeThreshold
+        
+    
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            if shouldDismissCardRight {
+                self.frame = CGRect(x: 1000, y: 0, width: self.frame.width, height: self.frame.height)
+            } else if shouldDismissCardLeft {
+                self.frame = CGRect(x: -1000, y: 0, width: self.frame.width, height: self.frame.height)
+            } else {
+                self.transform = .identity
+            }
+        }) { (_) in
             self.transform = .identity
-        }, completion: nil)
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
