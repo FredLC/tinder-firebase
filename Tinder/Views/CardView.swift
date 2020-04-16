a4b84e366bdca31970bced292e9c70dd9dac58fb
fredlc <fredlefevre@outlook.com>
2020-04-16 10:29:00 -0400

    Implemented cards deck view with pan gesture and animation

---
+++ Tinder/Views/CardView.swift
@@ -0,0 +1,56 @@
+//
+//  CardView.swift
+//  Tinder
+//
+//  Created by Fred Lefevre on 2020-04-16.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded()
+        default:
+            ()
+        }
+    }
+    
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    
+    fileprivate func handleEnded() {
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.3, options: .curveEaseOut, animations: {
+            self.transform = .identity
+        }, completion: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
